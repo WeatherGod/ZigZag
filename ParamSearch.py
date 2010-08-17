db340857addbf743125af65058692b671242ec83
Ben Root <ben.v.root@gmail.com>
2010-08-17 16:56:14 -0500

    Preliminary code for a ParamSearch script that will help find optimal parameters for trackers.

---
+++ ParamSearch.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from MultiAnalysis import MultiAnalyze
+from MultiTracking import MultiTrack
+
+if __name__ == '__main__' :
+    import argparse
+    import os           # for os.sep
+    from configobj import ConfigObj
+
+    parser = argparse.ArgumentParser(description='Search for optimal parameters for a tracker for a given multi-sim')
+    parser.add_argument("simName",
+                      help="Analyze tracks for SIMNAME",
+                      metavar="SIMNAME")
+    parser.add_argument("tracker", help="The tracker to use", metavar="TRACKER")
+    parser.add_argument("trackconfs", nargs='+',
+                      help="Config files for the parameters for the trackers",
+                      metavar="CONF")
+
+    args = parser.parse_args()
+
+    trackConfs = ConfigObj()
+    for file in args.trackconfs :
+        partConf = ConfigObj(file)
+        trackConfs.merge(partConf)
+
+    # Eliminate all but the one tracker I want
+    trackConfs = {args.tracker: trackConfs.pop(args.tracker)}
+
+    paramVals = [0.9999, 0.999, 0.99, 0.9]
+    for val in paramVals :
+        trackConfs[args.tracker]['pod'] = val
+        # TODO: still need to modify the destination of the result files
+        MultiTrack("%s%sMultiSim.ini" % (args.simName, os.sep), trackConfs.dict())
+
+
