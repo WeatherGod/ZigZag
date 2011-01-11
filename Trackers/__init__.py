314458884325a7cfd188b7bde5b2f095297cd46d
Ben Root <ben.v.root@gmail.com>
2011-01-11 17:14:29 -0600

    Added parameter validation scheme for tracker parameters. This will be part of the process for producing prettier graphs.

--- Trackers/__init__.py
+++ Trackers/__init__.py
@@ -5,11 +5,15 @@ import os
 
 
 trackerList = {}
-def _register_tracker(tracker, name) :
+param_confList = {}
+def _register_tracker(tracker, name, param_conf) :
     if name in trackerList :
         raise ValueError("%s is already a registered tracker." % name)
 
+    param_conf['algorithm'] = "string()"
     trackerList[name] = tracker
+    param_confList[name] = param_conf
+    
 
 
 
@@ -40,7 +44,7 @@ def SCIT_Track(trackRun, simParams, trackParams, returnResults=True, path='.') :
     if returnResults :
         return strmTracks, falarms
 
-_register_tracker(SCIT_Track, "SCIT")
+_register_tracker(SCIT_Track, "SCIT", dict(speedThresh="float(min=0.0)"))
 
 def MHT_Track(trackRun, simParams, trackParams, returnResults=True, path='.') :
     progDir = "~/Programs/mht_tracking/tracking/"
@@ -75,5 +79,21 @@ def MHT_Track(trackRun, simParams, trackParams, returnResults=True, path='.') :
     if returnResults :
         return TrackUtils.FilterMHTTracks(*TrackFileUtils.ReadTracks(dirName + os.sep + trackParams['result_file'] + "_" + trackRun))
 
-_register_tracker(MHT_Track, "MHT")
+_register_tracker(MHT_Track, "MHT", dict(varx="float(min=0.0, default=1.0)",
+                                         vary="float(min=0.0, default=1.0)",
+                                         vargrad="float(min=0.0, default=0.01)",
+                                         varint="float(min=0.0, default=100.0)",
+                                         varproc="float(min=0.0, default=0.5)",
+                                         pod="float(min=0.0, default=0.9999)",
+                                         lambdax="float(min=0.0, default=20.0)",
+                                         ntps="float(min=0.0, default=0.004)",
+                                         mfaps="float(min=0.0, default=0.0002)",
+                                         mxghpg="integer(min=1, default=300)",
+                                         mxdpth="integer(min=0, default=3)",
+                                         mnratio="float(min=0.0, default=0.001)",
+                                         intthrsh="float(min=0.0, default=0.90)",
+                                         mxdist="float(min=0.0, default=5.9)",
+                                         varvel="float(min=0.0, default=200.0)",
+                                         frames="integer(min=1, default=999999)",
+                                         ParamFile="string(default='Parameters')"))
 
