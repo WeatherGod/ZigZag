44136b194f68af721627373beff2a3281cddb141
Ben Root <ben.v.root@gmail.com>
2011-02-17 12:14:06 -0600

    Added some more items to the track reports.

--- scripts/TrackReports.py
+++ scripts/TrackReports.py
@@ -19,21 +19,34 @@ def main(args) :
 
         dists = [np.sum(np.hypot(*np.diff(segs))) for segs in trackSegs]
         lens = [np.ptp(aTrack['frameNums']) + 1 for aTrack in tracks]
-#        directions = [np.arctan2(np.sum(segs[1]/np.hypot(*segs))/len(segs),
-#                                 np.sum(segs[0]/np.hypot(*segs))/len(segs))
-#                      for segs in trackSegs]
+        startFrames = [np.min(aTrack['frameNums']) for aTrack in tracks]
+        endFrames = [np.max(aTrack['frameNums']) for aTrack in tracks]
+
+        import matplotlib.pyplot as plt
+        plt.scatter(lens, dists)
+        plt.xlabel('Frame Cnt')
+        plt.ylabel('Distance')
+        plt.xlim(min(lens), max(lens))
+        plt.ylim(min(dists), max(dists))
 
         print
         print afile, ':'
         print "Track Cnt: %4d" % len(tracks)
         print "False Alarm Cnt: %4d" % len(falarms)
+        #print "Clutter Points (estimated): %4d" % len([dist for dist, frameCnt in zip(dists, lens) if (dist < (0.5*np.mean(dists)) and frameCnt > np.mean(lens))])
 
         print "Max  Length: %4d Frames, %8.3f km" % (np.max(lens), np.max(dists))
         print "Mean Length: %4d Frames, %8.3f km" % (np.mean(lens), np.mean(dists))
+        print "Median Len : %4d Frames, %8.3f km" % (np.median(lens), np.median(dists))
         print "Min  Length: %4d Frames, %8.3f km" % (np.min(lens), np.min(dists))
+
+        print "Mean   Start: Frame %4d    End: Frame %4d" % (np.mean(startFrames), np.mean(endFrames))
+        print "Median Start: Frame %4d    End: Frame %4d" % (np.median(startFrames), np.median(endFrames))
 
         print "Weighted Mean Bearing: %6.2f degrees" % np.rad2deg(np.arctan2(np.sum(flatWeights * flatSegs[0]),
                                                                              np.sum(flatWeights * flatSegs[1])))
+
+        plt.show()
         
         
         
