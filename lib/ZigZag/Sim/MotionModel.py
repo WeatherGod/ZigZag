0d41cea53f4ab5df53d177f98c13d97317215240
Ben Root <ben.v.root@gmail.com>
2012-01-27 16:28:07 -0600

    New motion model!
    * An "truer" constant velocity model with random perturbations of
      the velocity by angle and speed.
    * Also added notes to the original constant velocity model emphasizing that
      it isn't a true constant velocity model (only piece-wise), and that users
      are likely wanting ConstVel2_Model, instead.

--- lib/ZigZag/Sim/MotionModel.py
+++ lib/ZigZag/Sim/MotionModel.py
@@ -22,6 +22,10 @@ class ConstVel_Model(MotionModel) :
     def __init__(self, deltaFrame, velModify) :
         """
         Create a new piece-wise constant velocity motion model.
+        ..note:
+            Well, this isn't exactly a constant velocity model. You
+            probably really want ConstVel2_Model.  This is kept for
+            backwards compatibility.
 
         This model changes randomly changes the speed of the track
              according to a uniform distribution with a range of +/- velModify.
@@ -51,3 +55,51 @@ class ConstVel_Model(MotionModel) :
 _motion_register(ConstVel_Model, 'ConstVel_Model', dict(deltaFrame="integer",
                                                         velModify="float(min=0)"))
 
+class ConstVel2_Model(MotionModel) :
+    def __init__(self, deltaFrame, angleModify, spdModify) :
+        """
+        Create a new constant velocity motion model.
+
+        This model applies random perturbations to the instantaneous speed and
+             direction of the track according to a uniform distribution
+             with a range of +/- angleModify and +/- spdModify.
+
+        deltaFrame : int
+            The frame increment
+
+        angleModify : float
+            The variance (+/-) of the uniform noise to apply to the direction
+            of the track at each iteration of the track.
+            This should be in units of degrees per unit time.
+
+        spdModify : float
+            The variance (+/-) of the uniform noise to apply to the magnitude
+            speed of the track at each iteration of the track.
+            This should be in units of distance per (unit time)**2
+        """
+        MotionModel.__init__(self)
+        self.deltaFrame = deltaFrame
+        self.accelTerm = spdModify
+        self.angleTerm = np.radians(angleModify)
+
+    def __call__(self, deltaT, xSpeed, ySpeed) :
+        dx = self.deltaFrame * deltaT * xSpeed
+        dy = self.deltaFrame * deltaT * ySpeed
+
+        angleModify = self.angleTerm * deltaT
+        dAngle = np.random.uniform(-angleModify, angleModify)
+        spdModify = self.accelTerm * deltaT
+        dSpd = np.random.uniform(-spdModify, spdModify)
+
+        dVelx = dSpd * np.cos(dAngle)
+        dVely = dSpd * np.sin(dAngle)
+
+        dx += dVelx * deltaT * self.deltaFrame
+        dy += dVely * deltaT * self.deltaFrame
+
+        return self.deltaFrame, dx, dy, 0.0, 0.0
+
+_motion_register(ConstVel2_Model, 'ConstVel2_Model',
+                 dict(deltaFrame="integer",
+                      angleModify="float(min=0, max=360)",
+                      spdModify="float(min=0)"))
